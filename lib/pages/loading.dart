bd08a16a3d672ce93caf10ed820844c77d11a10b
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-07-31 14:33:38 +0100

    #2 - Implement worldtime api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,10 +9,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'London', flag: 'germany.png', url:'Europe/London');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Loading Screen')
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time),
+      )
     );
   }
 }
