e41ad9ace5711009c52ac390bf75790d4e7d5855
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-08-06 09:33:54 +0100

    Formatting and showing dates

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,19 +18,42 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: [
-              TextButton.icon(
-                  onPressed: (){
-                    Navigator.pushNamed(
-                      context,
-                      '/location'
-                    );
-                  },
-                  label: const Text('Edit Location'),
-                  icon: const Icon(Icons.edit_location),
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: [
+                TextButton.icon(
+                    onPressed: (){
+                      Navigator.pushNamed(
+                        context,
+                        '/location'
+                      );
+                    },
+                    label: const Text('Edit Location'),
+                    icon: const Icon(Icons.edit_location),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                        data['location'],
+                        style: const TextStyle(
+                          fontSize: 28,
+                          letterSpacing: 2
+                        ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  data['time'],
+                  style: const TextStyle(
+                    fontSize: 66
+                  ),
+                )
+              ],
+            ),
           )
       ),
     );
